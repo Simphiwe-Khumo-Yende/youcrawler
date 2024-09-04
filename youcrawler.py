72093ebd7cc980be45911ebeabdf8770d5a1955c
syende023 <syende023@student.wethinkcode.co.za>
2024-09-04 12:57:36 +0200

    fixed dir layout

--- youcrawler.py
+++ youcrawler.py
@@ -92,4 +92,4 @@ def download_file(filename):
     return send_from_directory('raw_data', filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
